34839e290e8fe3b091e4af2887ad3e2dc7b61ea3
NikitaAnpilogov <nikanp8@gmail.com>
2024-12-07 21:44:29 +0300

    Version 11

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -3,128 +3,87 @@ package managers;
 import tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private LinkedMap linkedMap;
+    private HashMap<Integer, Node> history;
+    private Node first;
+    private Node last;
 
     public InMemoryHistoryManager() {
-        linkedMap = new LinkedMap();
+        history = new HashMap<>();
     }
 
     @Override
     public ArrayList<Task> getHistory() {
-        return linkedMap.getTasks();
+        return getTasks();
     }
 
     @Override
     public <T extends Task> void add(T task) {
-        Task taskHistory = (Task) task;
-        if (linkedMap.getSize() == 0) {
-            linkedMap.addHead(task);
-        } else {
-            linkedMap.linkLast(task);
-        }
+        Task historyTask = (Task) task;
+        linkLast(historyTask);
     }
 
     @Override
     public void remove(int id) {
-        linkedMap.remove(id);
-    }
-}
-
-/*class LinkedMap {
-    private HashMap<Integer, Node> history = new HashMap<>();
-    int lastId = -1;
-
-    void addHead(Task task) {
-        Node node = new Node(task);
-        history.put(task.getId(), node);
-        lastId = task.getId();
+        removeNode(id);
     }
 
-    void linkLast(Task task) {
-        if (task.getId() == lastId) {
-            return;
-        }
-        Node node = new Node(lastId, task);
-        //history.put(task.getId(), node);
-        Node anotherNode = history.get(lastId);
-        anotherNode.setIdNextTask(task.getId());
-        lastId = task.getId();
-        if (history.containsKey(task.getId())) { // Убираем повторы, если есть
-            Node deletedNode = history.get(task.getId());
-            if (deletedNode.getIdLastTask() == null && deletedNode.getIdNextTask() == null) {
-                history.remove(task.getId());
-            } else if (deletedNode.getIdLastTask() == null) { // Проверка на голову списка
-                Integer next = deletedNode.getIdNextTask();
-                Node nextNode = history.get(next);
-                nextNode.setIdLastTask(null);
-                history.remove(task.getId());
-            } else if (deletedNode.getIdNextTask() == null) { // Проверка на хвост
-                Integer last = deletedNode.getIdLastTask();
-                Node lastNode = history.get(last);
-                lastNode.setIdNextTask(null);
-                history.remove(task.getId());
-                lastId = lastNode.getIdTask();
-            } else { // Проверка на тело списка
-                Integer next = deletedNode.getIdNextTask();
-                Integer last = deletedNode.getIdLastTask();
-                Node nextNode = history.get(next);
-                Node lastNode = history.get(last);
-                nextNode.setIdLastTask(lastNode.getIdTask());
-                lastNode.setIdNextTask(nextNode.getIdTask());
-                history.remove(task.getId());
+    private void linkLast(Task task) {
+        if (last != null) {
+            if (task.equals(last.getTask())) {
+                return;
             }
         }
-        history.put(task.getId(), node);
-    }
-
-    ArrayList<Task> getTasks() {
-        ArrayList<Task> taskHistory = new ArrayList<>();
-        Integer next = lastId;
-        Task task;
-        if (history.size() == 0) {
-            return taskHistory;
+        final Node node = new Node(last, task);
+        if (first == null) {
+            first = node;
         } else {
-            for (int i = 0; i < history.size(); i++) {
-                Node node = history.get(next);
-                task = node.getTask();
-                if (node.getIdLastTask() != null) {
-                    next = node.getIdLastTask();
-                }
-                taskHistory.add(task);
-            }
+            last.setNodeNext(node);
+        }
+        last = node;
+        if (history.containsKey(task.getId())) {
+            removeNode(task.getId());
+        }
+        history.put(task.getId(), node);
+    }
+
+    private ArrayList<Task> getTasks() {
+        ArrayList<Task> taskHistory = new ArrayList<>();
+        Node node = last;
+        while (node != null) {
+            taskHistory.add(node.getTask());
+            node = node.getNodeLast();
         }
         return taskHistory;
     }
 
-    int getSize() {
-        return history.size();
-    }
-
-    void remove(int id) {
-        Node deletedNode = history.get(id);
-        if (deletedNode.getIdLastTask() == null && deletedNode.getIdNextTask() == null) {
-            history.remove(id);
-        } else if (deletedNode.getIdLastTask() == null) { // Проверка на голову списка
-            Integer next = deletedNode.getIdNextTask();
-            Node nextNode = history.get(next);
-            nextNode.setIdLastTask(null);
-            history.remove(id);
-        } else if (deletedNode.getIdNextTask() == null) { // Проверка на хвост
-            Integer last = deletedNode.getIdLastTask();
-            Node lastNode = history.get(last);
-            lastNode.setIdNextTask(null);
-            history.remove(id);
-            lastId = lastNode.getIdTask();
-        } else { // Проверка на тело списка
-            Integer next = deletedNode.getIdNextTask();
-            Integer last = deletedNode.getIdLastTask();
-            Node nextNode = history.get(next);
-            Node lastNode = history.get(last);
-            nextNode.setIdLastTask(lastNode.getIdTask());
-            lastNode.setIdNextTask(nextNode.getIdTask());
-            history.remove(id);
+    private void removeNode(int id) {
+        if (history.containsKey(id)) {
+            Node deletedNode = history.get(id);
+            if (deletedNode.getNodeLast() == null && deletedNode.getNodeNext() == null) {
+                history.remove(id);
+            } else if (deletedNode.getNodeLast() == null) { // Проверка на голову списка
+                Node nextNode = deletedNode.getNodeNext();
+                nextNode.setNodeLast(null);
+                history.remove(id);
+            } else if (deletedNode.getNodeNext() == null) { // Проверка на хвост
+                Node lastNode = deletedNode.getNodeLast();
+                lastNode.setNodeNext(null);
+                history.remove(id);
+                last = lastNode;
+            } else { // Проверка на тело списка
+                Node nextNode = deletedNode.getNodeNext();
+                Node lastNode = deletedNode.getNodeLast();
+                nextNode.setNodeLast(lastNode);
+                lastNode.setNodeNext(nextNode);
+                history.remove(id);
+            }
+        }
+        if (history.isEmpty()) {
+            last = null;
+            first = null;
         }
     }
-}*/
+}
--- src/managers/Node.java
+++ src/managers/Node.java
@@ -3,42 +3,42 @@ package managers;
 import tasks.Task;
 
 public class Node {
-    private Integer idLastTask;
-    private Integer idNextTask;
+    private Node nodeLast;
+    private Node nodeNext;
     private Task task;
 
-    public Node(int idLastTask, Task task) {
-        this.idLastTask = idLastTask;
+    public Node(Node idLastTask, Task task) {
+        this.nodeLast = idLastTask;
         this.task = task;
     }
 
-    public Node(Task task, int idNextTask) {
+    public Node(Task task, Node nodeNext) {
         this.task = task;
-        this.idNextTask = idNextTask;
+        this.nodeNext = nodeNext;
     }
 
     public Node(Task task) {
         this.task = task;
     }
 
-    public void setIdLastTask(Integer id) {
-        idLastTask = id;
+    public void setNodeLast(Node id) {
+        nodeLast = id;
     }
 
-    public void setIdNextTask(Integer id) {
-        idNextTask = id;
+    public void setNodeNext(Node id) {
+        nodeNext = id;
     }
 
-    public Integer getIdLastTask() {
-        return idLastTask;
+    public Node getNodeLast() {
+        return nodeLast;
     }
 
     public Task getTask() {
         return task;
     }
 
-    public Integer getIdNextTask() {
-        return idNextTask;
+    public Node getNodeNext() {
+        return nodeNext;
     }
 
     public Integer getIdTask() {
