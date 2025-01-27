2eafc84569fbea2c5792c0c7ead1c3bf93194611
NikitaAnpilogov <nikanp8@gmail.com>
2025-01-27 21:48:45 +0300

    Version 15.2

--- src/server/LocalDateTimeAdapter.java
+++ src/server/LocalDateTimeAdapter.java
@@ -2,12 +2,12 @@ package server;
 
 import com.google.gson.*;
 import java.lang.reflect.Type;
-import java.time.LocalDateTime;
+import java.time.LocalDateTime; // Не могу понять почему, но LocalDateTimeAdapter реализованный как на обучении не работал
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-// Не могу понять почему, но LocalDateTimeAdapter реализованный как на обучении не работал
+
     @Override
     public JsonElement serialize(LocalDateTime localDateTime, Type typeOfSrc, JsonSerializationContext context) {
         return new JsonPrimitive(localDateTime.format(formatter));
